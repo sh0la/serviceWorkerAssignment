c51ef69a20f56699c50d3f107103cfa83a03287d
sh0la <sholaadeniyi@gmail.com>
2020-01-08 11:36:30 -0600

    Add activate event listener to service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let cacheName = 'lorem_v1';
+let cacheName = 'lorem_v22';
 
 self.addEventListener('install', event => {
   
@@ -10,7 +10,7 @@ self.addEventListener('install', event => {
           'contact_us.html',
           'index.html',
           'staff.html',
-          'main.js'
+          'main.js',
         ]);
       })
       .catch(error => {
@@ -21,6 +21,26 @@ self.addEventListener('install', event => {
 
 
 self.addEventListener('fetch', event => {
-  
-  event.respondWith(caches.match(cacheName))
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request)
+      })
+  )
 })
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          };
+        })
+      );
+    })
+  );
+});
