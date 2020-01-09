904a0de605bf32e7f3fa42dcb3b06aba20c159b8
erfun-tavak <erfuntavakoli1396@gmail.com>
2020-01-08 18:12:43 -0600

    fix errors

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-let cacheName = 'lorem_v1';
+const cacheName = 'v1';
+const CacheFiles = [
+  './about_us.html',
+  './contact_us.html',
+  './index.html',
+  './staff.html',
+  './main.js',
+  './styles/about_us.css',
+  './styles/contact_us.css',
+  './styles/header.css',
+  './styles/index.css',
+  './styles/normalize.css',
+  './styles/staff.css'
+];
 
 self.addEventListener('install', event => {
-  
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
-        cache.addAll([
-          './about_us.html',
-          './contact_us.html',
-          './index.html',
-          './staff.html',
-          './main.js',
-          './styles/about_us.css',
-          './styles/contact_us.css',
-          './styles/header.css',
-          './styles/index.css',
-          './styles/normalize.css',
-          './styles/staff.css'
-        ]);
+        cache.addAll(CacheFiles);
       })
       .catch(error => {
          console.log(error);
@@ -40,15 +40,13 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         if (response) {
-          console.log('this was in cache')
           return response
         };
-        console.log('not cache')
+
         return fetch(event.request)
       })
   )
